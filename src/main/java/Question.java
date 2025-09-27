43bfa1c7615ace6b6fdbb5741e5c753e3405cb3f
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-09-27 18:17:38 +0300

    replace individual option fields with List<String> options, simplify getCorrectAnswer() method logic, add default constructor for Jackson deserialization

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -1,27 +1,22 @@
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class Question {
 
-    private final String boxerImage;
-    private final String firstOption;
-    private final String secondOption;
-    private final String thirdOption;
-    private final String fourthOption;
-    private final int trueAnswer;
+    private String image;
+    private List<String> options;
+    private int trueAnswer;
 
     public String getCorrectAnswer() {
-        return switch (this.trueAnswer) {
-            case 1 -> this.getFirstOption();
-            case 2 -> this.getSecondOption();
-            case 3 -> this.getThirdOption();
-            case 4 -> this.getFourthOption();
-            default -> "";
-        };
+        return options.get(trueAnswer - 1);
     }
 
 }
